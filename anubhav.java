bf0120a4b21e227c83123a0adb766d6e94cac7cb
Anubhav Mishra <mishraanu870@gmail.com>
2021-01-06 00:47:10 +0530

    First commit

---
+++ anubhav.java
@@ -0,0 +1,33 @@
+import java.util.regex.Pattern; 
+import java.util.*;
+ class Exp { 
+  
+    
+    static String reverseWords(String str) 
+    { 
+  
+      
+        Pattern pattern = Pattern.compile("\\s"); 
+  
+        
+        String[] temp = pattern.split(str); 
+        String result = ""; 
+   
+        for (int i = 0; i < temp.length; i++) { 
+            if (i == temp.length - 1) 
+                result = temp[i] + result; 
+            else
+                result = " " + temp[i] + result; 
+        } 
+        return result; 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+      Scanner sc= new Scanner(System.in);
+    String s1=sc.nextLine();
+        System.out.println(reverseWords(s1)); 
+  
+        
+    } 
+}
---
+++ first.java
@@ -0,0 +1,11 @@
+public class first
+{
+    public static void main(String[] args) {
+        int n = 8;
+        for (int i = 0; i <=10; i++) 
+        {
+            System.out.print(n*i);            
+            System.out.print(" ");            
+        }
+    }
+}
